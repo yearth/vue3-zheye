2401bd58807661a537817f6356576efd1f1b176a
wujunlong <yearthmain@gmail.com>
2020-11-20 17:40:23 +0800

    [feature]: 初始化项目
    1. 配置 prettier，解决和 eslint 的冲突

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  stylelintIntegration: true,
+  eslintIntegration: true,
+  insertPragma: false,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
+}
